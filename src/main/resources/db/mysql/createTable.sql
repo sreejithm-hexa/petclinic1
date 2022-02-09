888b069a299abee27556c1889336a25fa9e33ec4
sreejithm-hexa <84178325+sreejithm-hexa@users.noreply.github.com>
2022-02-09 18:57:20 +0530

    Delete createTable.sql

--- src/main/resources/db/mysql/createTable.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---changeset author:Sreejith splitStatements:true endDelimiter:;
-create table TEST_1 ( ID int not null, FNAME varchar(100) not null);
-create table TEST_2 ( ID int not null, FNAME varchar(100) not null);
-create table TEST_3 ( ID int not null, FNAME varchar(100) not null);
-create table TEST_4 ( ID int not null, FNAME varchar(100) not null);
