727fc448dbcf636cf6bd592e2c484a75f94b9c3e
sreejithm-hexa <84178325+sreejithm-hexa@users.noreply.github.com>
2022-02-09 18:40:45 +0530

    Update data.sql

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+
+--changeset author:Sreejith splitStatements:true endDelimiter:;
 INSERT IGNORE INTO vets VALUES (1, 'James', 'Carter');
 INSERT IGNORE INTO vets VALUES (2, 'Helen', 'Leary');
 INSERT IGNORE INTO vets VALUES (3, 'Linda', 'Douglas');
