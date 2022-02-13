9a0ecea4f898909c69f5d65e87edb68cd39fd680
sreejithm-hexa <84178325+sreejithm-hexa@users.noreply.github.com>
2022-02-13 22:00:45 +0530

    Create 004-version.sql

---
+++ src/main/resources/db/mysql/004-version.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+
+--changeset author:hari splitStatements:true endDelimiter:; context:test
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
